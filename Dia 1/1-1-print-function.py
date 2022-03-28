dbe1af55cd06d7130b28412bf5c8d41e7166b1d4
Paulo Vinicius Oliveira Santos <paulovinicius_1973@hotmail.com>
2022-03-27 22:09:14 -0300

    Day 1 Files

---
+++ Dia 1/1-1-print-function.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+#Python Bootcamp - Day 1
+#Python print function
+#Author: Paulo Vinícius
+#Date: 27.03.2022
+
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('What to print.')")
